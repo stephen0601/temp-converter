0ec20c8012aa08ab228b0a481c7faaee89816878
stephen0601 <100398250+stephen0601@users.noreply.github.com>
2022-04-09 22:08:51 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -9,8 +9,8 @@ while continue_ != False:
     print(f"{unconverted}° fahrenheit is {rounded_temp}° celsius")
     readstring_ = input("Do you want to continue? (yes/no) ") #if user wants to continue, program will run again
     if readstring_ == "yes": 
-        continue_ = True #one equal sign sets continue_ EQUAL to true
-    if readstring_ == "no": # == is a boolean operator.
+        continue_ = True 
+    if readstring_ == "no": 
         continue_ = False
         print("Please run me again!")
         exit()
