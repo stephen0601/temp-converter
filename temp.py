772e0527b5ffe1d870351fae18bd9eec12f658fb
stephen0601 <100398250+stephen0601@users.noreply.github.com>
2022-04-09 22:07:28 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,12 +1,16 @@
 continue_ = True
+readstring_ = ""
+
+
 while continue_ != False:
     unconverted = float(input("Hello, please type in your degrees in fahrenheit : ")) 
-    convert_value = (unconverted - 32) * 5/9 #equation for conversion
-    print(f"{unconverted}° fahrenheit is {convert_value}° celsius") #fstring to plug values
-    continue_ = input("Do you want to continue? (yes/no)") #if user wants to continue, program will run again
-if continue_ == 'yes': 
-    continue_ == True
-if continue_ == 'no':
-    continue_ == False
-    print("Please run me again!")
-    exit()
+    convert_value = (unconverted - 32) * 5/9
+    rounded_temp = int(convert_value)
+    print(f"{unconverted}° fahrenheit is {rounded_temp}° celsius")
+    readstring_ = input("Do you want to continue? (yes/no) ") #if user wants to continue, program will run again
+    if readstring_ == "yes": 
+        continue_ = True #one equal sign sets continue_ EQUAL to true
+    if readstring_ == "no": # == is a boolean operator.
+        continue_ = False
+        print("Please run me again!")
+        exit()
